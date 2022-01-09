b6b4763f4615a958af1fd4b4424fac9bd9b9c815
Tomohiro Kida <kida@libra.local>
2022-01-09 12:13:36 +0900

    modify day05

--- day05.rb
+++ day05.rb
@@ -1,23 +1,9 @@
 require "pp"
-def inputCoord
-  cs = []
-  while 1 do
-    c = []
-    line = gets
-    break if line == nil
-    tmp = line.chomp.split(" ")
-    c << tmp.first.split(",").map(&:to_i)
-    c << tmp.last.split(",").map(&:to_i)
-    cs << c
-  end
-  return cs
-end
+
 def calMapSize(coords)
   xsize = 0
   ysize = 0
-  coords.each do |coord|
-    x1, y1 = coord.first # [0, 9]
-    x2, y2 = coord.last  # [5, 9]
+  coords.each_cons(2) do |(x1, y1), (x2, y2)|
     xsize = [x1, x2, xsize].max
     ysize = [y1, y2, ysize].max
   end
@@ -28,9 +14,7 @@ def createMap(coords)
   xsize, ysize = calMapSize(coords)
   p xsize, ysize
   map = Array.new(ysize) { Array.new(xsize, 0) }
-  coords.each do |coord|
-    x1, y1 = coord.first # [0, 9]
-    x2, y2 = coord.last  # [5, 9]
+  coords.each_cons(2) do |(x1, y1), (x2, y2)|
     y1.step(y2, (y2-y1)/(y2-y1).abs) { |y| map[y][x1] += 1 } if x1 == x2 
     x1.step(x2, (x2-x1)/(x2-x1).abs) { |x| map[y1][x] += 1 } if y1 == y2
     if x1 != x2 && y1 != y2
@@ -45,8 +29,7 @@ def createMap(coords)
   end
   return map
 end
-coords = inputCoord
-#p coords
+p coords = File.read("day05_input_test.txt").scan(/(\d+),(\d+)/).map {|l| l.map(&:to_i) }
 map = createMap(coords)
 pp map
 p map.flatten.select {|n| n >= 2 }.size
