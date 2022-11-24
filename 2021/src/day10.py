f6929a40a078bbc42fa790b19d7103b9c615fa5c
TomohiroKida <tomo426.sakura1281m@gmail.com>
2022-11-25 01:03:56 +0900

    success day10-2

--- 2021/src/day10.py
+++ 2021/src/day10.py
@@ -22,22 +22,57 @@ def syntax_checker(line):
       if len(stack) == 0:
         exit()
       if not is_complete(stack.pop(), chunk):
-        return chunk
-  return ''
+        return chunk, stack
+  return '', stack
 
-def solve(lines):
+def solve1(lines):
   illegals = []
   for line in lines:
-    char = syntax_checker(line)
-    if char != '':
-      illegals.append(char)
-  point_list = {')': 3, ']': 57, '}': 1197, '>': 25137}
-  points = [point_list[illegal] for illegal in illegals]
+    #print(line)
+    res, _ = syntax_checker(line)
+    #print(res)
+    if res != '':
+      illegals.append(res)
+  point_table = {')': 3, ']': 57, '}': 1197, '>': 25137}
+  points = [point_table[illegal] for illegal in illegals]
   print(sum(points))
 
+def open2close(open_chunk):
+  match open_chunk:
+    case '(': return ')'
+    case '[': return ']'
+    case '{': return '}'
+    case '<': return '>'
+
+def solve2(lines):
+  scores = []
+  for line in lines:
+    res , stack = syntax_checker(line)
+    if res != '':
+      continue
+
+    adding = list(map(open2close, stack))
+    adding.reverse()
+    #print(adding)
+    point_table = {')': 1, ']': 2, '}': 3, '>': 4}
+    point_adding = [point_table[x] for x in adding]
+
+    # calc score
+    score = 0
+    #print(point_adding)
+    for value in point_adding:
+      score = score * 5 + value
+    #print(score)
+    scores.append(score)
+
+  # print middle score
+  scores.sort()
+  #print(scores)
+  print(scores[int((len(scores)-1)/2)])
+    
 import sys
 with open(sys.argv[1], "r") as f:
   inputs = f.readlines()
 lines = [x.strip() for x in inputs]
-print(lines)
-solve(lines)
+solve1(lines)
+solve2(lines)
