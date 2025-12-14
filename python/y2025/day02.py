499ff177601ba80b447815664fa904d2ea13cc4b
TomohiroKida <tomo426.sakura1281m@gmail.com>
2025-12-14 20:11:07 +0900

    fin day02

--- python/y2025/day02.py
+++ python/y2025/day02.py
@@ -9,17 +9,25 @@ def part1(input_data: str):
   #print(data)
   cnt = 0
   for a, b in data:
-    for i in range(a, b+1, 1):
-      s = len(str(i))
-      if s % 2 != 0: continue
-    
-      s1 = str(i)[:int(s/2)]
-      s2 = str(i)[int(s/2):]
-      if s1 == s2: cnt += i
+    for n in range(a, b+1):
+      s = str(n)
+      w = s[:int(len(s)/2)]
+      if s == w*2: cnt += n
   return cnt
 
 def part2(input_data: str):
-  None
+  data = parse(input_data)
+  cnt = 0
+  for a, b in data:
+    for n in range(a, b+1):
+      s = str(n)
+      for i in range(1, int(len(s)/2)+1):
+        w = s[:i]
+        if s == w*int(len(s)/i):
+          cnt += n
+          break
+  return cnt
+
 
 # *** TESTCASE ***
 import unittest
@@ -37,7 +45,8 @@ class TestSolution(unittest.TestCase):
   def test_part2(self):
     input_str, exp = (
 """
-""", None)
+11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124
+""", 4174379265)
     self.assertIsNotNone(exp)
 
     act = part2(input_str)
